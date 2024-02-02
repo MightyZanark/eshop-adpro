797c0c697880a8891149b54adeb98dd29dc10881
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-02-02 23:26:14 +0900

    feat: added edit functionality to repository and service

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -5,18 +5,43 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private Map<String, Integer> productIdToIdx = new HashMap<>();
 
     public Product create(Product product) {
+        String id = UUID.randomUUID().toString();
+        product.setProductId(id);
         productData.add(product);
+        productIdToIdx.put(id, productData.size() - 1);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    private int getProductIndex(String productId) {
+        Integer index = productIdToIdx.get(productId);
+        if (index == null)
+            throw new NoSuchElementException("Product with id " + productId + " does not exists");
+        return index;
+    }
+
+    public Product find(String productId) {
+        Integer index = getProductIndex(productId);
+        return productData.get(index);
+    }
+
+    public void edit(Product newProduct) {
+        Integer index = getProductIndex(newProduct.getProductId());
+        productData.set(index, newProduct);
     }
 }
