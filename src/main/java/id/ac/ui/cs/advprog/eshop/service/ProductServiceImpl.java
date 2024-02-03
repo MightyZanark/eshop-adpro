c4d322c3b794b4f9c7d136879e08c9eba7325837
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-02-03 19:49:19 +0900

    fix: ignore null values

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -25,7 +25,10 @@ public class ProductServiceImpl implements ProductService {
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
-        productIterator.forEachRemaining(allProduct::add);
+        productIterator.forEachRemaining((product) -> {
+            if (product != null)
+                allProduct.add(product);
+        });
         return allProduct;
     }
 
