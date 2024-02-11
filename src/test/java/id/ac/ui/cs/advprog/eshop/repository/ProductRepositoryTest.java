9b58bb80ee2da1e7eac1353e2bd5d8fa67c70607
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-02-11 18:23:28 +0900

    feat: add repository test to get 100% coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -88,5 +87,28 @@ class ProductRepositoryTest {
         assertDoesNotThrow(() -> productRepository.find(product2.getProductId()));
         productRepository.delete(product2.getProductId());
         assertThrows(NoSuchElementException.class, () -> productRepository.find(product2.getProductId()));
+    }
+
+    @Test
+    void testCreateAfterDelete() {
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        assertDoesNotThrow(() -> productRepository.find(product1.getProductId()));
+        assertEquals(0, productRepository.getProductIndex(product1.getProductId()));
+        productRepository.delete(product1.getProductId());
+        assertThrows(NoSuchElementException.class, () -> productRepository.find(product1.getProductId()));
+        
+        Product product2 = new Product();
+        product2.setProductId("a0f9de46-98b1-437d-a0bf-d0821dde9096");
+        product2.setProductName("Sampo Cap Usep");
+        product2.setProductQuantity(50);
+        productRepository.create(product2);
+        
+        assertDoesNotThrow(() -> productRepository.find(product2.getProductId()));
+        assertEquals(0, productRepository.getProductIndex(product2.getProductId()));
     }
 }
