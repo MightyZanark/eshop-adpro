670ad1d640c55793ac483a650da1175627c2ee78
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-02-11 18:22:57 +0900

    feat: ignore main application from JaCoCo reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,4 +74,12 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        files(classDirectories.files.map {
+            fileTree(it) {
+                exclude("**/EshopApplication.class")
+            }
+        })
+    )
 }
