8bd21f6f53f0555dde9cdaee61abbd664697fd82
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2024-03-06 22:36:11 +0900

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
@@ -13,11 +12,11 @@ import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> paymentData;
-    private Map<String, Order> paymentOrder;
+    private List<Payment> paymentData = new ArrayList<>();
+    private Map<String, Order> paymentOrder = new HashMap<>();
 
     public Payment save(Order order, Payment payment) {
-        payments.add(payment);
+        paymentData.add(payment);
         paymentOrder.put(payment.getId(), order);
         return payment;
     }
